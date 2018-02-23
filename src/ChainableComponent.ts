010716bc31ddb43874af4bd1f5fdf14615aed4cf
Paul Gray <pfbgray@gmail.com>
2018-02-23 00:24:43 -0500

    Refactoring chainable implementation

--- src/ChainableComponent.ts
+++ src/ChainableComponent.ts
@@ -1,53 +1,47 @@
-import { createFactory } from 'react';
+import { createFactory, ReactNode } from 'react';
+
+export type ChainableComponent<A> = {
+  ap(f: (a: A) => ReactNode): ReactNode;
+  map<B>(f: (a: A) => B): ChainableComponent<B>;
+  chain<B>(f: (a: A) => ChainableComponent<B>): ChainableComponent<B>;
+}
 
 export type RenderPropsProps<P, A> = P & {
-  children: (a: A) => JSX.Element
+  children: (a: A) => ReactNode
 };
 
 export type RenderPropsComponent<P, A> = React.ComponentClass<RenderPropsProps<P, A>>;
 
-type Applied<A> = (f:(a:A) => JSX.Element) => JSX.Element;
-
-function toChainable<A>(f: Applied<A>): ChainableComponent<A> {
-  return ({
-    ap(f2: (a: A) => JSX.Element): JSX.Element {
-      return f(f2);
-    },
-    map<B>(f: (a: A) => B): ChainableComponent<B> {
-      const Mapped: Applied<B> = g => this.ap(a => g(f(a)));
-      return toChainable(Mapped);
-    },
-    chain<B>(f: (a: A) => ChainableComponent<B>): ChainableComponent<B> {
-      const FlatMapped: Applied<B> = g => this.ap(a => f(a).ap(g));
-      return toChainable(FlatMapped);
-    }
-  });
-}
+type Applied<A> = (f:(a:A) => ReactNode) => ReactNode;
 
 // P is the configuration parameter to build a chainable component
 export function buildChainable<P extends object, A>(Inner: RenderPropsComponent<P, A>): (p:P) => ChainableComponent<A> {
-  //return a => new ChainableComponentWrapper(a, inner);
-  return p => ({
-    ap(f: (a: A) => JSX.Element): JSX.Element {
-      const apply = createFactory<RenderPropsProps<P, A>>(Inner);
-      return apply({
-        ...(p as any), // todo: we have any until https://github.com/Microsoft/TypeScript/pull/13288 is merged
-        children: f
-      });
-    },
+   return p => fromAp(f => {
+    const apply = createFactory<RenderPropsProps<P, A>>(Inner);
+    return apply({
+      ...(p as any), // todo: we have any until https://github.com/Microsoft/TypeScript/pull/13288 is merged
+      children: f
+    });
+   });
+}
+
+
+function fromAp<A>(ap: (f: (a: A) => ReactNode) => ReactNode): ChainableComponent<A> {
+  return {
+    ap,
     map<B>(f: (a: A) => B): ChainableComponent<B> {
       const Mapped: Applied<B> = g => this.ap(a => g(f(a)));
-      return toChainable(Mapped);
+      return fromAp(Mapped);
     },
     chain<B>(f: (a: A) => ChainableComponent<B>): ChainableComponent<B> {
       const FlatMapped: Applied<B> = g => this.ap(a => f(a).ap(g));
-      return toChainable(FlatMapped);
+      return fromAp(FlatMapped);
     }
-  });
+  };
 }
 
-export interface ChainableComponent<A> {
-  ap(f: (a: A) => JSX.Element): JSX.Element; // it shouldn't be any, it should be void
-  map<B>(f: (a: A) => B): ChainableComponent<B>;
-  chain<B>(f: (a: A) => ChainableComponent<B>): ChainableComponent<B>;
+const ChainableComponent = {
+  of<A>(a: A): ChainableComponent<A> {
+    return fromAp(f => f(a));
+  }
 }
