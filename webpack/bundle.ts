1489eddae8dadae26263c227e45478a80bd35711
Paul Gray <pfbgray@gmail.com>
2018-06-11 18:18:46 -0400

    1.2.0

--- webpack/bundle.ts
+++ webpack/bundle.ts
@@ -1,20 +1,12 @@
-import * as webpack from 'webpack';
 import config from './base';
 
 const bundleConfig = {
   ...config,
+  optimization: {
+    minimize: true
+  },
   plugins: [
     ...(config.plugins || []),
-    new webpack.DefinePlugin({
-      // This is a macro substitution; it has to end up in the source with quotes.
-      'process.env.NODE_ENV': '"production"',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-    }),
   ],
 };
 
