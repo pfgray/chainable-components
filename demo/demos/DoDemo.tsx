c3945efe273096e3ac911f26886db0676e20d763
Paul Gray <pfbgray@gmail.com>
2018-08-01 09:40:26 -0400

    Adding Do method to combine chainables

---
+++ demo/demos/DoDemo.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { withState } from '../../src/lib/withState';
+import { ChainableComponent } from '../../src/ChainableComponent';
+import Step from '../Step';
+
+export const DoDemo =
+  ChainableComponent.Do(
+    withState(5),
+    (outer) => withState(2 + outer.value),
+    (outer, inner) => ({outer, inner})
+  )
+  .render(({outer, inner}) => (
+    <div>
+      <div>outer: {outer.value}</div>
+      <button onClick={() => outer.update(outer.value + 1)}>+</button>
+
+      <div>inner: {inner.value}</div>
+      <button onClick={() => inner.update(inner.value + 1)}>+</button>
+    </div>
+  ));
+
+export default () => (
+  <Step title="Do Demo">
+    <pre className='code-sample'>
+      {`import { withState, ChainableComponent } from 'chainable-components';
+
+ChainableComponent.Do(
+  withState(5),
+  (outer) => withState(2 + outer.value),
+  (outer, inner) => ({outer, inner})
+)
+.render(({outer, inner}) => (
+  <div>
+    <div>outer: {outer.value}</div>
+    <button onClick={() => outer.update(outer.value + 1)}>+</button>
+
+    <div>inner: {inner.value}</div>
+    <button onClick={() => inner.update(inner.value + 1)}>+</button>
+  </div>
+));`}
+    </pre>
+    {DoDemo}
+  </Step>
+);
